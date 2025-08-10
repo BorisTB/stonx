266ccb08f49afba2f71d927f549e2418aba0cd42
Boris Polak <18208654+BorisTB@users.noreply.github.com>
2025-08-10 14:07:48 +0200

    test(elysia): try to fix nx e2e tests on CI
    another try

--- e2e/elysia/jest.config.ts
+++ e2e/elysia/jest.config.ts
@@ -1,10 +1,21 @@
 /* eslint-disable */
 import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+let dir;
+let __filename;
+
+if (typeof __dirname === 'undefined') {
+  // @ts-ignore
+  __filename = fileURLToPath(import.meta.url);
+  __dirname = dirname(__filename);
+} else {
+  dir = __dirname;
+}
 
 // Reading the SWC compilation config for the spec files
-const swcJestConfig = JSON.parse(
-  readFileSync(`${__dirname}/.spec.swcrc`, 'utf-8')
-);
+const swcJestConfig = JSON.parse(readFileSync(`${dir}/.spec.swcrc`, 'utf-8'));
 
 // Disable .swcrc look-up by SWC core because we're passing in swcJestConfig ourselves
 swcJestConfig.swcrc = false;
